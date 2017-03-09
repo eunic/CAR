27e6eea1a8374ed31f65da884b16dc4af298f7a9
Eunice <victorywanjiru@gmail.com>
2017-03-09 12:00:28 +0300

    This is my commit for the Car OOP class

---
+++ carclass.py
@@ -0,0 +1,54 @@
+class Car(object):
+  
+
+  def __init__(self, name='General', model='GM', car_type=None,speed = 0):
+    
+    self.name = name
+    self.model = model
+    self.car_type = car_type
+    self.speed = speed
+
+
+    if self.name in ['Porshe', 'Koenigsegg']:
+      
+       self.num_of_doors = 2
+      
+    else:
+      
+      self.num_of_doors = 4
+      
+      
+
+    if self.is_saloon() is False:
+      
+      self.num_of_wheels = 8
+      
+    else:
+      
+      self.num_of_wheels = 4
+
+
+  def is_saloon(self):
+    
+    if self.car_type == 'trailer':
+
+       return False
+
+    else:
+        
+       return True
+        
+  
+
+  def drive(self, speed):
+     
+        if  self.is_saloon() is False:
+          
+            self.speed = speed * 11
+            
+        else:
+          
+            self.speed = 10 ** speed
+
+        return Car()
+
